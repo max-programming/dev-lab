6ae01863c3341d10fb5e48d09136701d2573e0c6
Usman S. <ui.sabuwala@gmail.com>
2025-03-08 19:33:21 +0000

    first commit

---
+++ app/routes/__root.tsx
@@ -0,0 +1,49 @@
+import type { PropsWithChildren } from "react";
+import {
+  Outlet,
+  createRootRoute,
+  HeadContent,
+  Scripts,
+} from "@tanstack/react-router";
+import rootCss from "@/root.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "DevLab",
+      },
+    ],
+    links: [{ rel: "stylesheet", href: rootCss }],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: PropsWithChildren) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
